2ffcc8063acc16759b11f41c181e349c0655c972
seunggyundev <91270887+seunggyundev@users.noreply.github.com>
2022-11-04 03:26:28 +0900

    Initial Commit

---
+++ multi_thread_alarm/multi_thread_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_thread_alarmApp.swift
+//  multi_thread_alarm
+//
+//  Created by 장승균 on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct multi_thread_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ multi_thread_alarmTests/multi_thread_alarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  multi_thread_alarmTests.swift
+//  multi_thread_alarmTests
+//
+//  Created by 장승균 on 2022/11/04.
+//
+
+import XCTest
+@testable import multi_thread_alarm
+
+final class multi_thread_alarmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
